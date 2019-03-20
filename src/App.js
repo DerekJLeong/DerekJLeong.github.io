598437798a51cfbedb8dada42a33066674d27b2a
Derek J Leong <derekjleong@gmail.com>
2019-03-19 21:47:48 -0700

    Feat: Added Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/about";
 import Projects from "./components/projects";
 import Skills from "./components/skills";
 import Contact from "./components/contact";
+import Footer from "./components/footer";
 import "./App.css";
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
                <Skills />
                <Contact />
             </main>
+            <Footer />
          </React.Fragment>
       );
    }
---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+   state = {};
+   render() {
+      return <nav className="footer">Footer</nav>;
+   }
+}
+
+export default Footer;
