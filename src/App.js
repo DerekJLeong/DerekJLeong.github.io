f5c204068041618e9c5f18a38fa7995893d6a353
Derek J Leong <derekjleong@gmail.com>
2019-03-31 13:52:02 -0700

    feat: projectContainer and descriptions

--- src/App.js
+++ src/App.js
@@ -14,21 +14,24 @@ class App extends Component {
          {
             id: 1,
             title: "Portfolio Site V1",
-            description: "",
+            description:
+               "My original portfolio site, made using vanilla HTML, CSS, and JavaScript. Rebuilt and designed after discoving ReactJS to display new abilities.",
             img: require("./components/images/portfolioProject.png"),
             link: ""
          },
          {
             id: 2,
-            title: "Concentration Game",
-            description: "",
+            title: "Concentration Memory Game",
+            description:
+               "Simple game based off of the classic Concentration. Demonstration of JavaScript DOM manipulation, event handling, and CSS animations.",
             img: require("./components/images/concentrationProject.png"),
             link: ""
          },
          {
             id: 3,
             title: "Restuarant Review Application",
-            description: "",
+            description:
+               "1 of 8 projects completed for Udacity Front End Nanodegree. Responsible for responsive redesign using CSS flex box/grid, ARIA accessibility, and service worker offline caching for multi-page application.",
             img: require("./components/images/restuarantProject.png"),
             link: ""
          }
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,14 +4,17 @@ import "./project.css";
 class Project extends Component {
    render() {
       return (
-         <div className="project">
-            <div className="projectHeader">
-               <span class="headerDot dot1" />
-               <span class="headerDot dot2" />
-               <span class="headerDot dot3" />
-               <h2>{this.props.project.title}</h2>
+         <div className="projectContainer">
+            <div className="project">
+               <div className="projectHeader">
+                  <span class="headerDot dot1" />
+                  <span class="headerDot dot2" />
+                  <span class="headerDot dot3" />
+                  <h2>{this.props.project.title}</h2>
+               </div>
+               <img src={this.props.project.img} alt="project" />
             </div>
-            <img src={this.props.project.img} alt="project" />
+            <p>{this.props.project.description}</p>
          </div>
       );
    }
