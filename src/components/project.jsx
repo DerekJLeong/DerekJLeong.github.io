4947f49b70e8316b277da06b7e759076961e7e49
Derek J Leong <derekjleong@gmail.com>
2019-03-31 14:28:58 -0700

    feat; Project hover functionality, needs styling

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,7 +4,7 @@ import "./project.css";
 class Project extends Component {
    render() {
       return (
-         <div className="projectContainer">
+         <div className={this.getContainerClass()}>
             <div className="project">
                <div className="projectHeader">
                   <span class="headerDot dot1" />
@@ -12,11 +12,32 @@ class Project extends Component {
                   <span class="headerDot dot3" />
                   <h2>{this.props.project.title}</h2>
                </div>
-               <img src={this.props.project.img} alt="project" />
+               <div>
+                  <img src={this.props.project.img} alt="project" />
+                  <div class={this.getTargetClasses()}>
+                     <h2>{this.props.project.title}</h2>
+                     <ul>
+                        <li>Demo</li>
+                        <li>GitHub</li>
+                     </ul>
+                  </div>
+               </div>
             </div>
             <p>{this.props.project.description}</p>
          </div>
       );
+   }
+
+   getContainerClass() {
+      let containerClass = "projectContainer hoverContainer-";
+      containerClass += this.props.project.id;
+      return containerClass;
+   }
+
+   getTargetClasses() {
+      let targetClasses = "projectHover hoverTarget-";
+      targetClasses += this.props.project.id;
+      return targetClasses;
    }
 }
 
