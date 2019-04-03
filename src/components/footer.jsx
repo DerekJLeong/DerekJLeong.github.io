8092a673dfc4daa0c7ca6d4fc46d2aa7e7cc8613
Derek J Leong <derekjleong@gmail.com>
2019-04-02 18:57:39 -0700

    feat: footer styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class Footer extends Component {
    state = {};
    render() {
-      return <footer className="footer">Footer</footer>;
+      return (
+         <footer className="footer">
+            <p>© 2019 Developed by Derek J Leong</p>
+         </footer>
+      );
    }
 }
 
