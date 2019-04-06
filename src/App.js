bb72bf6e878c7a1c41022daf2a91a7c85ddee1cf
Derek J Leong <derekjleong@gmail.com>
2019-04-05 20:01:14 -0700

    feat: project section 768px media query styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,15 @@ class App extends Component {
       projects: [
          {
             id: 1,
+            title: "Portfolio Site V2",
+            description:
+               "This website! Made using React.js using JSX and pure CSS. It uses react-scrollable-anchor, lightweight library for scrollable anchor tags. Completely designed and build by myself.",
+            img: require("./components/images/portfolioProject.png"),
+            demo: "",
+            github: "https://github.com/DerekJLeong/DerekJLeong.github.io"
+         },
+         {
+            id: 2,
             title: "Portfolio Site V1",
             description:
                "My original portfolio site, made using vanilla HTML, CSS, and JavaScript. Rebuilt and designed after discoving React.js to display new abilities.",
@@ -22,7 +31,7 @@ class App extends Component {
             github: "https://github.com/DerekJLeong/DerekJLeong.github.io"
          },
          {
-            id: 2,
+            id: 3,
             title: "Concentration Memory Game",
             description:
                "1 of 8 different projects completed for Udacity Front End Nanodegree. Based off of the classic game 'Concentration'. Demonstration of JavaScript DOM manipulation, event handling, and CSS animations.",
@@ -32,7 +41,7 @@ class App extends Component {
                "https://github.com/DerekJLeong/Udacity-FE-Memory-Game/tree/master/Front%20End%20Project%20-%20Memory%20Game"
          },
          {
-            id: 3,
+            id: 4,
             title: "Restuarant Review Application",
             description:
                "Another project completed as part of Front End Nanodegree. Responsible for responsive redesign using CSS flex box/grid, accessibility, and service worker offline caching for multi-page application.",
