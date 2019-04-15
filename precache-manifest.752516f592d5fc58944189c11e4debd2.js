c1face77b32350cfe3e4841d90b7f0719fb42180
Derek J Leong <derekjleong@gmail.com>
2019-04-14 21:14:44 -0700

    Updates

---
+++ precache-manifest.752516f592d5fc58944189c11e4debd2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "7791e048ad069171729195369839d785",
+    "url": "/static/media/email.7791e048.svg"
+  },
+  {
+    "revision": "c00cc40002246cd22f06",
+    "url": "/static/css/main.2dec5c4a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5c9f7d5f0cde52f089c3",
+    "url": "/static/js/2.a706d8b6.chunk.js"
+  },
+  {
+    "revision": "6436c0ee8f80e8f506d2e91ab341b27a",
+    "url": "/static/media/portfolioProject.6436c0ee.png"
+  },
+  {
+    "revision": "c9dd2d822161750846ba89f48bff423d",
+    "url": "/static/media/downloadSmall.c9dd2d82.svg"
+  },
+  {
+    "revision": "4387ad742a2b4f488e5329d3785a00f6",
+    "url": "/static/media/placeholder.4387ad74.png"
+  },
+  {
+    "revision": "d21d1246dc0c9ab4c7fb27e5201b4eea",
+    "url": "/static/media/logo.d21d1246.png"
+  },
+  {
+    "revision": "71608f7e81967e868f805c7650da8f5b",
+    "url": "/static/media/down-arrow.71608f7e.svg"
+  },
+  {
+    "revision": "eb7bb4f2e9d01c1ba9e7253da276b12e",
+    "url": "/static/media/close.eb7bb4f2.svg"
+  },
+  {
+    "revision": "bc781d3f57a0d6bcf39a52421f35b73e",
+    "url": "/static/media/menu.bc781d3f.svg"
+  },
+  {
+    "revision": "c00cc40002246cd22f06",
+    "url": "/static/js/main.dd777152.chunk.js"
+  },
+  {
+    "revision": "9ec04d4ef6b2f4a4eaa91e85fd1ee050",
+    "url": "/static/media/linkedin.9ec04d4e.svg"
+  },
+  {
+    "revision": "782a504b67cff114fea27bb927e8a0eb",
+    "url": "/static/media/github.782a504b.svg"
+  },
+  {
+    "revision": "9e610a16eb6c0ddc2646ae650e23d8cb",
+    "url": "/static/media/download.9e610a16.svg"
+  },
+  {
+    "revision": "0d80cf99fdec8f37dac5d1ede3ba4844",
+    "url": "/static/media/restuarantProject.0d80cf99.png"
+  },
+  {
+    "revision": "d20deb59edd193019ee772dd6cc88f95",
+    "url": "/static/media/concentrationProject.d20deb59.png"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/static/media/css3.6971ddc7.svg"
+  },
+  {
+    "revision": "7fd504665575a4ea5ed926fbfa839f68",
+    "url": "/static/media/javascript.7fd50466.svg"
+  },
+  {
+    "revision": "a0f3643b36bfe79fb3f6d93a48e61497",
+    "url": "/static/media/think.a0f3643b.svg"
+  },
+  {
+    "revision": "23021b261de75cd6d384aa1bbfbdb031",
+    "url": "/index.html"
+  }
+];
